2980b13d6fbe95ec0251a3780277fdff6dc81b66
Yadasko <valentin.citera@dilepix.com>
2018-12-22 14:07:44 +0100

    Changed every statement to preparedStatements (when needed).

--- src/fr/acceis/forum/metier/Datafetcher_sqlite.java
+++ src/fr/acceis/forum/metier/Datafetcher_sqlite.java
@@ -48,25 +48,27 @@ public class Datafetcher_sqlite implements Datafetcher {
 
 	@Override
 	public List<Message> fetchAssociatedMessages(int thread_id) throws SQLException {
-		Statement stmt = this.connection.createStatement();
 
 		String SQL =  "SELECT MESSAGES.content, Users.login, Users.id, Threads.name, (SELECT COUNT(*) FROM MESSAGES WHERE author_id = Users.id) AS msg_count" + 
-					" FROM Messages, Threads  INNER JOIN USERS ON MESSAGES.author_id = USERS.id AND MESSAGES.thread_id = Threads.ID WHERE MESSAGES.thread_id = " + thread_id + " ORDER BY MESSAGES.id";
-		System.out.println("Executing SQL: " + SQL);
-
-		ResultSet result = stmt.executeQuery( SQL );
+					" FROM Messages, Threads  INNER JOIN USERS ON MESSAGES.author_id = USERS.id AND MESSAGES.thread_id = Threads.ID WHERE MESSAGES.thread_id = ? ORDER BY MESSAGES.id";
+		PreparedStatement ps = this.connection.prepareStatement(SQL);
+		
+		ps.setInt(1, thread_id);
+		
+		ResultSet result = ps.executeQuery();
 
 		List<Message> messages = new ArrayList<Message>();
 
 		while(result.next()) {
 			messages.add(new Message(0, result.getInt("id"), result.getString("content"), 0, result.getString("login"), result.getInt("msg_count"), result.getString("name")));
 		}
-		stmt.close();
+		ps.close();
 		return messages;
 	}
 
 	@Override
 	public List<Thread> fetchThreads() throws SQLException {
+		/* Pas d'input utilisateur, pas besoin de passer par des prepared statements */
 		ArrayList<Thread> list = new ArrayList<Thread>();
 		Statement stmt = this.connection.createStatement();
 
@@ -88,44 +90,43 @@ public class Datafetcher_sqlite implements Datafetcher {
 
 	@Override
 	public User fetchUser(String login) throws SQLException {
-		Statement stmt = this.connection.createStatement();
-
-		String SQL =  "SELECT Users.id, Users.login, Users.password, (SELECT COUNT(*) FROM MESSAGES WHERE author_id = Users.id) AS msg_count FROM Users WHERE login = '" + login + "'";
-		System.out.println("Executing SQL: " + SQL);
-
-		ResultSet result = stmt.executeQuery( SQL );
-
+		String SQL =  "SELECT Users.id, Users.login, Users.password, (SELECT COUNT(*) FROM MESSAGES WHERE author_id = Users.id) AS msg_count FROM Users WHERE login = ? ";
+		PreparedStatement ps = this.connection.prepareStatement(SQL);
+		
+		ps.setString(1, login);
+		
+		ResultSet result = ps.executeQuery();
 
 		if (result.next()) {
 			User u = new User(result.getString("login"), result.getInt("id"), result.getString("password"));
 			u.setPosts_number(result.getInt("msg_count"));
-			stmt.close(); // Sqlite differs from hsqldb as we can't close the statement one the query is done but once we have fetched everything
+			ps.close(); // Sqlite differs from hsqldb as we can't close the statement once the query is done but once we have fetched everything
 			return u;
 		}
 		else {
-			stmt.close();
+			ps.close();
 			// Return fake user. We know that id = -1 means no user has been found
 			return new User("NONE", -1, "NONE");
 		}
 	}
 
 	public User fetchUser(int id) throws SQLException {
-		Statement stmt = this.connection.createStatement();
 		
-		String SQL =  "SELECT Users.id, Users.login, Users.password, (SELECT COUNT(*) FROM MESSAGES WHERE author_id = Users.id) AS msg_count FROM Users WHERE id = '" + id + "'";
-		System.out.println("Executing SQL: " + SQL);
+		String SQL =  "SELECT Users.id, Users.login, Users.password, (SELECT COUNT(*) FROM MESSAGES WHERE author_id = Users.id) AS msg_count FROM Users WHERE id = ? ";
+		PreparedStatement ps = this.connection.prepareStatement(SQL);
+		
+		ps.setInt(1, id);
 
-		ResultSet result = stmt.executeQuery( SQL );
-
+		ResultSet result = ps.executeQuery();
 
 		if (result.next()) {
 			User u = new User(result.getString("login"), result.getInt("id"), result.getString("password"));
 			u.setPosts_number(result.getInt("msg_count"));
-			stmt.close(); // Sqlite differs from hsqldb as we can't close the statement one the query is done but once we have fetched everything
+			ps.close(); // Sqlite differs from hsqldb as we can't close the statement once the query is done but once we have fetched everything
 			return u;
 		}
 		else {
-			stmt.close();
+			ps.close();
 			// Return fake user. We know that id = -1 means no user has been found
 			return new User("NONE", -1, "NONE");
 		}
@@ -145,30 +146,34 @@ public class Datafetcher_sqlite implements Datafetcher {
 	
 	@Override
 	public void updateThreadViewCount(int thread_id) throws SQLException {
-		Statement stmt = this.connection.createStatement();
-
-		String SQL =  "UPDATE THREADS SET  views = views + 1 WHERE id = " + thread_id;
-		System.out.println("Executing SQL: " + SQL);
-
-		stmt.execute( SQL );
-		stmt.close();
+		String SQL =  "UPDATE THREADS SET  views = views + 1 WHERE id = ?";
+		PreparedStatement ps = this.connection.prepareStatement(SQL);
+		
+		ps.setInt(1, thread_id);
+		
+		ps.execute();
+		ps.close();
 	}
 
 	@Override
 	public void updateThreadViewCount(int thread_id, int view_count) throws SQLException {
-		Statement stmt = this.connection.createStatement();
+		
 
-		String SQL =  "UPDATE THREADS SET  views = " + view_count + " WHERE id = " + thread_id;
+//		String SQL =  "UPDATE THREADS SET  views = " + view_count + " WHERE id = " + thread_id;
+		String SQL =  "UPDATE THREADS SET views = ? WHERE id = ? ";
+		PreparedStatement ps = this.connection.prepareStatement(SQL);
+		
+		ps.setInt(1, view_count);
+		ps.setInt(2, thread_id);
+		
 		System.out.println("Executing SQL: " + SQL);
 
-		stmt.execute( SQL );
-		stmt.close();
+		ps.execute();
+		ps.close();
 	}
 
 	@Override
 	public void addThreadAnswer(int thread_id, String content, int user_id) throws SQLException {
-		Statement stmt = this.connection.createStatement();
-		
 		String SQL = "INSERT INTO `Messages`(`author_id`,`content`,`thread_id`) VALUES (?, ?, ?);";
 		PreparedStatement ps = this.connection.prepareStatement(SQL);
 		
